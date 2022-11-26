35eeb4792666bbeadba22afa5eb663db53658f7f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-26 14:16:39 -0300

    added connection to elephant sql db

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -3,15 +3,9 @@ import 'reflect-metadata';
 
 import { DataSource } from 'typeorm';
 
-const port = process.env.DB_PORT as number | undefined;
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: port,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URI,
   entities: [`${__dirname}/**/entities/*{ts,js}`],
   migrations: [`${__dirname}/**/migrations/*{ts,js}`]
 });
