2efba728993bdebb387e9318d4c6ff72b8ca7408
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-26 15:31:25 -0300

    added cors to solve Access-Control-Allow-Origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import cors from 'cors';
+
 import { AppDataSource } from './data-source';
 import routes from './routes';
 
 AppDataSource.initialize().then(() => {
   const app = express();
+
+  app.use(cors({origin: '*'}));
 
   app.use(express.json());
 
