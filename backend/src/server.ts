f6e2259dca5bba21a969599aa9864f556023c6ed
ayanfaustt <ayanfaustt@gmail.com>
2022-11-26 12:51:35 -0300

    feat: implementação do cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,22 @@
-import express  from "express";
+import express from "express";
 import { database } from "./database";
 import "dotenv/config";
 import { router } from "./routes/routes";
-
+import cors from "cors";
 
 const server = express();
 server.use(express.json());
 server.use(router);
+server.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
 
+const port = process.env.SERVER_PORT;
 
-const port = process.env.SERVER_PORT
-
-server.listen(port || 3001, async()=>{
-    await database.sync();
-    console.log("Servidor rodando...")
+server.listen(port || 3001, async () => {
+  await database.sync();
+  console.log("Servidor rodando...");
 });
