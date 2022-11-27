2a7e0b14a08caccb14795c0b756c8cc4981d2081
Dgley Ramos <dgleysilva@gmail.com>
2022-11-27 17:29:45 -0300

    Subindo dados

--- api/index.ts
+++ api/index.ts
@@ -7,7 +7,7 @@ const path = require('path');
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
-const INDEX = './build/index.html';
+const INDEX = '../../client/build/index.html';
 
 AppDataSource.initialize().then(() => {
     const app = express()
