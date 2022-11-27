ac3a1f7322e4b11e8594f13f4a9ebf366db36570
Dgley Ramos <dgleysilva@gmail.com>
2022-11-27 16:06:34 -0300

    Teste de configurações

--- api/index.ts
+++ api/index.ts
@@ -13,12 +13,12 @@ AppDataSource.initialize().then(() => {
     app.use(express.json())
 
     // Fazer com que o Node sirva os arquivos do app em React criado
-    app.use(express.static(path.resolve(__dirname, '../client/build')));
+    app.use(express.static(path.resolve(__dirname, './client/build')));
 
     app.use(routes)
     // Todas as outras solicitações GET não tratadas retornarão nosso app em React
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
     });
 
     return app.listen(process.env.PORT)
