cb6a16ec136f4ffed0ee6f704f2c8abdf559c0dc
Dgley Ramos <dgleysilva@gmail.com>
2022-11-27 17:51:18 -0300

    Alterando index.ts do server

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 
 
+import bodyParser from "body-parser";
 import express from "express"
 import { AppDataSource } from './src/data-source';
 import routes from './src/routes'
@@ -7,24 +8,28 @@ const path = require('path');
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
-const INDEX = '../../client/build/index.html';
 
 AppDataSource.initialize().then(() => {
     const app = express()
+
+    // Body parser middleware
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
     app.use(cors())
 
     app.use(express.json())
 
     app.use(routes)
 
-    if(process.env.NODE_ENV) {
-        //static folder add
-        app.use(express.static('client/build'));
-        
-        app.get("*", function (req, res) {
-            res.sendFile(path.resolve(__dirname , "client/build", "index.html"));
-        });
-    }
+    // Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('/*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
     return app.listen(PORT)
 })
