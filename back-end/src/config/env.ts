7b2a2fea503c5e2cfc26ee76635e4b195e5d0f3e
Jonathan <jonathan.wpc@gmail.com>
2022-12-06 17:35:25 -0300

    feat: install and configure sequelize orm

--- back-end/src/config/env.ts
+++ back-end/src/config/env.ts
@@ -2,6 +2,11 @@ import "dotenv/config";
 
 const ENV = {
   PORT: process.env.PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
 }
 
 export default ENV;
