68af1d089e1b9b0f241d49e3a9f8cec7779eda1c
Jonathan <jonathan.wpc@gmail.com>
2022-12-07 20:36:05 -0300

    feat: add cors and express json parser

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 import ENV from "./config/env";
 import BaseRoutes from "./routes";
 
 const app = express();
 const port = ENV.PORT;
 
+app.use(cors());
+app.use(express.json());
 app.use(BaseRoutes);
 
 app.listen(port, () => {
