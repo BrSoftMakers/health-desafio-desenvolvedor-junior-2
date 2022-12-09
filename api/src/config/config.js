343ac71c2be27ace6f9e9aa70f972a35128011b2
alvesgf16 <alvesgabriel1994@gmail.com>
2022-12-09 17:25:24 -0300

    refactor: create one db for each env

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
+
 const config = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
   host: process.env.POSTGRES_HOST,
   dialect: 'postgres',
 };
 
 module.exports = {
-  development: config,
-  test: config,
-  production: config,
+  development: {
+    ...config,
+    database: 'health_development',
+  },
+  test: {
+    ...config,
+    database: 'health_test',
+    logging: false,
+  },
+  production: {
+    ...config,
+    database: 'health_production',
+  },
 };
