e682fe290cb824b4d098fdac583d90e7845e598a
Jonathan <jonathan.wpc@gmail.com>
2022-12-10 15:34:49 -0300

    feat: add and implemente main code files of app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import styles from './App.module.css';
+import PetList from './components/PetList/PetList';
+import PetModal from './components/PetModal/PetModal';
+import SectionUp from './components/SectionUp/SectionUp';
 
 const App = () => {
   return (
     <>
+      <div className={styles.container}>
+        <div className={styles.leftMenuSpace}></div>
+        <div className={styles.leftMenu}>
 
+        </div>
+        <main className={styles.main}>
+          <SectionUp />
+          <hr />
+          <section className={styles.pets}>
+            <h2>Pet List</h2>
+            <div>
+              <PetList />
+            </div>
+          </section>
+        </main>
+      </div>
     </>
   );
 }
