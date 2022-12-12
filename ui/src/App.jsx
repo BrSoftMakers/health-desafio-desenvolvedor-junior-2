d7959bc9217ae64fd9f1d046a10b9b450d9158ab
alvesgf16 <alvesgabriel1994@gmail.com>
2022-12-12 11:07:17 -0300

    feat: add header

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import './assets/colors.css';
 import './assets/typography.css';
 import Form from './components/Form';
+import Header from './components/Header';
 import Table from './components/Table';
 import { PetProvider } from './context/PetContext';
 
 function App() {
   return (
     <PetProvider>
+      <Header />
       <main className="main-section">
         <Form />
         <Table />
