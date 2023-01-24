ac8847aef3006279f4e8a86c8668862bc2c4b565
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 17:56:07 -0300

    :sparkles: feat: create service to creation of owners

---
+++ back-end/src/services/ownerService.ts
@@ -0,0 +1,18 @@
+import { owners } from "@prisma/client";
+
+import { repository, createOnwerProps } from "../repositories/ownerRepository";
+import { error } from "../utils/errorTypes";
+
+async function post(owner: createOnwerProps) {
+    const isCPFInUse = !!repository.findByCPF(owner.CPF);
+
+    if (isCPFInUse) {
+        throw error.conflict();
+    }
+
+    await repository.create(owner);
+}
+
+export const service = {
+    post,
+};
