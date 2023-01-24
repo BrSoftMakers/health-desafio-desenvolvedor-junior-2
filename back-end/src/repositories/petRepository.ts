a757343f915fd5a0cdb8efe38119f160043cf150
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 19:26:53 -0300

    :sparkles: feat: create function to update pet data

--- back-end/src/repositories/petRepository.ts
+++ back-end/src/repositories/petRepository.ts
@@ -3,6 +3,7 @@ import { pets } from "@prisma/client";
 import client from "../dbStrategy/postegresStrategy";
 
 export type postPetProps = Omit<pets, "id">;
+export type patchPetProps = Omit<pets, "id" | "ownerId">;
 
 async function create(petData: postPetProps) {
     await client.pets.create({ data: petData });
@@ -16,7 +17,17 @@ async function findAll() {
     });
 }
 
+async function findById(id: number) {
+    return await client.pets.findUnique({ where: { id } });
+}
+
+async function updatePetData(id: number, petData: patchPetProps) {
+    await client.pets.update({ data: petData, where: { id } });
+}
+
 export const repository = {
     create,
     findAll,
+    findById,
+    updatePetData,
 };
