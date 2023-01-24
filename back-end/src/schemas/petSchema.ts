18f79c169ef6f691a936ac52d9490656e6c849ef
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 19:25:50 -0300

    :sparkles: feat: create schema to update pet data

--- back-end/src/schemas/petSchema.ts
+++ back-end/src/schemas/petSchema.ts
@@ -7,3 +7,10 @@ export const postSchema = joi.object({
     breed: joi.string().trim().required(),
     ownerId: joi.number().id().required(),
 });
+
+export const patchSchema = joi.object({
+    name: joi.string().strict().trim(),
+    age: joi.number().positive(),
+    type: joi.string().strict().trim().valid("GATO", "CACHORRO"),
+    breed: joi.string().trim(),
+});
