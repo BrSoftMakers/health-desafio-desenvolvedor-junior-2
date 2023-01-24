aca8ff0f96471570c8fffacf0baec1c1f7c127b0
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 18:38:19 -0300

    :sparkles: feat: integrate /pets to app

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import onwerRoutes from "./ownerRoutes";
+import petRoutes from "./petRoutes";
 
 const routes = Router();
 
 routes.use(onwerRoutes);
+routes.use(petRoutes);
 
 export { routes };
