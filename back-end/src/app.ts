528364c8dfb1278375f1d75f98b401abdb4570c1
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-25 02:19:23 -0300

    :hammer: refactor: change index.ts to app.ts and refactor app from server to use in tests

---
+++ back-end/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+import "express-async-errors";
+import cors from "cors";
+
+import { loadEnv } from "./loadEnv";
+
+import { connectDb } from "./dbStrategy/postegresStrategy";
+import { redisConnect } from "./dbStrategy/redisStrategy";
+
+import errorHandler from "./middlewares/errorMiddleware";
+import { routes } from "./routes";
+
+loadEnv();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use(errorHandler);
+
+export async function init(): Promise<Express> {
+    connectDb();
+    await redisConnect();
+
+    return Promise.resolve(app);
+}
+
+export default app;
