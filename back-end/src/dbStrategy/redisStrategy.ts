86d6b7505c46e7449156ba3bdc40a9be51ac1018
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-25 03:24:08 -0300

    :sparkles: feat: create function to disconnect redis and centralize cache infos

--- back-end/src/dbStrategy/redisStrategy.ts
+++ back-end/src/dbStrategy/redisStrategy.ts
@@ -4,6 +4,15 @@ export const redis = createClient({
     url: process.env.REDIS_URL,
 });
 
+export const cachExpiration = 10000;
+export const cacheKeys = {
+    pets: "pets",
+};
+
 export async function redisConnect() {
     await redis.connect();
+}
+
+export async function redisDisconnect() {
+    await redis.disconnect();
 }
