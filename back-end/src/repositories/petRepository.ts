54792c3371ebf06cb9a3c4b4ed92c9502a23942a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 21:17:53 -0300

    :sparkles: feat: integrate redis in pets repository

--- back-end/src/repositories/petRepository.ts
+++ back-end/src/repositories/petRepository.ts
@@ -1,6 +1,7 @@
 import { pets } from "@prisma/client";
 
 import { client } from "../dbStrategy/postegresStrategy";
+import { redis } from "../dbStrategy/redisStrategy";
 
 export type postPetProps = Omit<pets, "id">;
 export type patchPetProps = Omit<pets, "id" | "ownerId">;
@@ -13,21 +14,40 @@ export type getPetProps = Omit<pets, "ownerId"> & {
     };
 };
 
+const cachKey = "pets";
+const cachExpiration = 10000;
+
 async function create(petData: postPetProps) {
     await client.pets.create({ data: petData });
 }
 
 async function findAll() {
-    return await client.pets.findMany({
-        select: {
-            age: true,
-            breed: true,
-            id: true,
-            name: true,
-            type: true,
-            owner: true,
-        },
-    });
+    let result;
+
+    try {
+        const cachedEvent = await redis.get(cachKey);
+
+        if (cachedEvent) {
+            result = JSON.parse(cachedEvent);
+        } else {
+            result = await client.pets.findMany({
+                select: {
+                    age: true,
+                    breed: true,
+                    id: true,
+                    name: true,
+                    type: true,
+                    owner: true,
+                },
+            });
+
+            redis.setEx(cachKey, cachExpiration, JSON.stringify(result));
+        }
+
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function findById(id: number) {
@@ -36,10 +56,14 @@ async function findById(id: number) {
 
 async function updatePetData(id: number, petData: patchPetProps) {
     await client.pets.update({ data: petData, where: { id } });
+
+    await redis.del(cachKey);
 }
 
 async function removeById(id: number) {
     await client.pets.delete({ where: { id } });
+
+    await redis.del(cachKey);
 }
 
 async function findByOwnerCPF(CPF: string) {
