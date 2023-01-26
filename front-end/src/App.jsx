0e77b8e750a6796a7f49a366d2a2bf9fa067917f
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 12:55:39 -0300

    :sparkles: feat: integrate routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Home from "./screens/home";
+import Pets from "./screens/pets/inde";
+import Register from "./screens/register";
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
-            <Routes></Routes>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/pets"
+                    element={<Pets />}
+                />
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+            </Routes>
         </BrowserRouter>
     );
 }
