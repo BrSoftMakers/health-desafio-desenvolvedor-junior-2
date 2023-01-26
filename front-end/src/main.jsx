ab86bc49ef09ecf8ef77ed5300d6f11b252346d6
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-25 21:57:37 -0300

    :sparkles: feat: integrate reset and global css in app

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./css/reset.css";
+import "./css/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
