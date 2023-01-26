73edc379b3243ea19fc5fbc81f0ec7e43a71658d
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 18:48:21 -0300

    :sparkles: feat: add state variable to disable select route button

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Logo from "../../assets/Logo.png";
@@ -5,6 +6,8 @@ import Logo from "../../assets/Logo.png";
 import { Container, Title, NavButton, FlexContainer } from "./style";
 
 export default function Header() {
+    const [pathName, setPathName] = useState("/");
+
     return (
         <Container>
             <FlexContainer>
@@ -19,14 +22,29 @@ export default function Header() {
 
             <FlexContainer>
                 <Link to="/">
-                    <NavButton>HOME</NavButton>
+                    <NavButton
+                        disable={window.location.pathname === "/"}
+                        onClick={() => setPathName(window.location.pathname)}
+                    >
+                        HOME
+                    </NavButton>
                 </Link>
                 <Link to="/pets">
-                    <NavButton>PETS</NavButton>
+                    <NavButton
+                        disable={window.location.pathname === "/pets"}
+                        onClick={() => setPathName(window.location.pathname)}
+                    >
+                        PETS
+                    </NavButton>
                 </Link>
 
                 <Link to="register">
-                    <NavButton>REGISTRAR</NavButton>
+                    <NavButton
+                        disable={window.location.pathname === "/register"}
+                        onClick={() => setPathName(window.location.pathname)}
+                    >
+                        REGISTRAR
+                    </NavButton>
                 </Link>
             </FlexContainer>
         </Container>
