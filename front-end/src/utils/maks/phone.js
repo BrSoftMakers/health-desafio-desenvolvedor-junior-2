afdafc54bd07bd94fc318124cc7d20db323771b1
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 17:13:18 -0300

    :sparkles: feat: create function to unmask phoneNumber

--- front-end/src/utils/maks/phone.js
+++ front-end/src/utils/maks/phone.js
@@ -1,6 +1,10 @@
-export function maskPhoneNumber(v) {
-    v = v.replace(/\D/g, "");
-    v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
-    v = v.replace(/(\d)(\d{4})$/, "$1-$2");
-    return v;
+export function maskPhoneNumber(phoneNumber) {
+    phoneNumber = phoneNumber.replace(/\D/g, "");
+    phoneNumber = phoneNumber.replace(/^(\d{2})(\d)/g, "($1) $2");
+    phoneNumber = phoneNumber.replace(/(\d)(\d{4})$/, "$1-$2");
+    return phoneNumber;
+}
+
+export function unMaskPhoneNumber(phoneNumber) {
+    return phoneNumber.replace("[^d]", "");
 }
