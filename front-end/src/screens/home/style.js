55ebff219a4039e934a205938ab415abfe4d8860
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 00:06:24 -0300

    :lipstick: feat: add animations to alternate between imgs

--- front-end/src/screens/home/style.js
+++ front-end/src/screens/home/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100vw;
-    height: 100vh;
+    height: 90vh;
 
     display: grid;
     grid-template-columns: 1fr 1fr;
@@ -17,14 +17,74 @@ export const ColumnWrapper = styled.div`
     align-items: center;
 
     max-width: 50vw;
+
+    position: relative;
 `;
 
 export const Heading = styled.h2`
-    font-size: 35px;
+    font-size: 60px;
+    font-family: "Lilita One", cursive;
     line-height: 130%;
     text-align: center;
 
-    color: blue;
+    color: #ffffff;
 
     width: 60%;
+
+    animation: tilt-shaking 5s infinite;
+
+    @keyframes tilt-shaking {
+        0% {
+            transform: rotate(0deg);
+        }
+        25% {
+            transform: rotate(5deg);
+        }
+        50% {
+            transform: rotate(0eg);
+        }
+        75% {
+            transform: rotate(-5deg);
+        }
+        100% {
+            transform: rotate(0deg);
+        }
+    }
+`;
+
+export const Image = styled.img`
+    @keyframes fade {
+        0% {
+            opacity: 1;
+        }
+        33% {
+            opacity: 0;
+        }
+        66% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+    @-webkit-keyframs fade {
+        0% {
+            opacity: 1;
+        }
+        33% {
+            opacity: 0;
+        }
+        66% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    -webkit-animation: fade 15s infinite;
+    animation: fade 15s infinite;
 `;
