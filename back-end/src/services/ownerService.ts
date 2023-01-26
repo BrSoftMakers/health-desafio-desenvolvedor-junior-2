c35d2480feb8f324cdebdec2a49dbb85b15b20f3
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 04:28:19 -0300

    :sparkles: feat: change condition to update ownerData if passed CPF is in use

--- back-end/src/services/ownerService.ts
+++ back-end/src/services/ownerService.ts
@@ -1,13 +1,10 @@
-import { owners } from "@prisma/client";
-
 import { repository, createOnwerProps } from "../repositories/ownerRepository";
-import { error } from "../utils/errorTypes";
 
 async function post(owner: createOnwerProps) {
     const isCPFInUse = !!(await repository.findByCPF(owner.CPF));
 
     if (isCPFInUse) {
-        throw error.conflict();
+        return await repository.update(owner);
     }
 
     return await repository.create(owner);
