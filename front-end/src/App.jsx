29b13105562b6519ba0a164601313ae2aa2f69f8
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-26 13:33:21 -0300

    :sparkles: feat: implements lazy loading

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,28 +1,35 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from "./screens/home";
-import Pets from "./screens/pets/inde";
-import Register from "./screens/register";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import("./screens/home"));
+const Pets = lazy(() => import("./screens/pets"));
+const Register = lazy(() => import("./screens/register"));
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
-                <Route
-                    path="/pets"
-                    element={<Pets />}
-                />
-                <Route
-                    path="/register"
-                    element={<Register />}
-                />
-            </Routes>
+            <Suspense fallback={<Loading height="90vh" />}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+
+                    <Route
+                        path="/pets"
+                        element={<Pets />}
+                    />
+
+                    <Route
+                        path="/register"
+                        element={<Register />}
+                    />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
