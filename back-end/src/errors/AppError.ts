1ec82b05f617defdb7a8c2e751538a36b5e6860d
wesleydematos <wesleydematos3@gmail.com>
2023-02-11 14:00:35 -0300

    feat: create handle error

---
+++ back-end/src/errors/AppError.ts
@@ -0,0 +1,9 @@
+export class AppError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
