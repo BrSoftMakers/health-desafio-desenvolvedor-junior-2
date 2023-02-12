6f9a19206d83e20424aa416693927c17866f39a1
wesleydematos <wesleydematos3@gmail.com>
2023-02-12 18:36:21 -0300

    fix: adjust Pets Page

--- front-end/src/Pages/Pets/index.tsx
+++ front-end/src/Pages/Pets/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { PetCard } from "../../components/PetCard/";
+import { iPet } from "../../components/PetCard/interfaces";
 import api from "../../services/api";
 
 export const Pets = () => {
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
-    api.get("").then((response) => {
+    api.get("/pets").then((response) => {
       console.log(response.data);
       setPets(response.data);
     });
@@ -16,6 +17,19 @@ export const Pets = () => {
   return (
     <>
       <Header />
+      {pets.map((pet: iPet) => (
+        <PetCard
+          age={pet.age}
+          breed={pet.breed}
+          id={pet.id}
+          name={pet.name}
+          phoneNumber={pet.phoneNumber}
+          species={pet.species}
+          tutorName={pet.tutorName}
+          image={pet.image}
+          key={pet.id}
+        />
+      ))}
     </>
   );
 };
