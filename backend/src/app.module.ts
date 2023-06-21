fcefe62f712c9c0ad26699d61656d1f96de0d2b3
Wdson Danrlei <wdson91@gmail.com>
2023-06-21 09:39:58 -0300

    att

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PetsModule } from './pets/pets.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', 'client'),
-  }),
+  imports: [
     AuthModule, PetsModule],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useStaticAssets(join(__dirname, '..', 'client'));
+
   app.enableCors();
   await app.listen(3000);
 }
