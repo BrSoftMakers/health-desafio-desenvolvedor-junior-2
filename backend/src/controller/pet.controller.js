bef437b8ba93956143760035c1bafa0f348e1694
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-01 16:02:24 -0300

    feat: adiciona tratamento de erro com try/cath

--- backend/src/controller/pet.controller.js
+++ backend/src/controller/pet.controller.js
@@ -2,11 +2,17 @@ const PetService = require('../service/pet.service');
 
 const createPet = async (req, res) => {
   try {
-    const { name, age, specie, breed, ownerId } = req.body;
-    const pet = await PetService.createPet({ name, age, specie, breed, ownerId });
+    const { name, age, species, breed, ownerId } = req.body;
+    const pet = await PetService.createPet({ name, age, species, breed, ownerId });
     return res.status(201).json(pet);
   } catch (error) {
-    return res.status(erro.status).json({ message: error.message });
+    if (!error.status) {
+      console.log(error);
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
+    return res
+      .status(error.status)
+      .json({ statusCode: error.status, message: error.message });
   }
 };
 
@@ -20,6 +26,10 @@ const getPetById = async (req, res) => {
     const pet = await PetService.getPetById(id);
     return res.status(200).json(pet);
   } catch (error) {
+    if (!error.status) {
+      console.log(error);
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
     return res.status(error.status).json({ message: error.message });
   }
 };
@@ -30,6 +40,10 @@ const getPetByOwnerId = async (req, res) => {
     const pet = await PetService.getPetByOwnerId(ownerId);
     return res.status(200).json(pet);
   } catch (error) {
+    if (!error.status) {
+      console.log(error);
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
     return res.status(error.status).json({ message: error.message });
   }
 };
@@ -41,6 +55,10 @@ const updatePet = async (req, res) => {
     const pet = await PetService.updatePet(id, name, age, specie, breed, ownerId);
     return res.status(200).json(pet);
   } catch (error) {
+    if (!error.status) {
+      console.log(error);
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
     return res.status(error.status).json({ message: error.message });
   }
 };
@@ -51,6 +69,10 @@ const deletePet = async (req, res) => {
     await PetService.deletePet(id);
     return res.status(204).end();
   } catch (error) {
+    if (!error.status) {
+      console.log(error);
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
     return res.status(error.status).json({ message: error.message });
   }
 };
