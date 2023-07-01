590833ec014adfc36ff10c39f2f3717c57b4730b
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-01 16:01:50 -0300

    feat: adiciona nova metodo para busca de dono de pet pelo documento

--- backend/src/controller/owner.controller.js
+++ backend/src/controller/owner.controller.js
@@ -2,14 +2,34 @@ const OwnerService = require('../service/owner.service');
 
 const createOwner = async (req, res, next) => {
   try {
-    const { fullName, phone } = req.body;
-    const owner = await OwnerService.createOwner({ fullName, phone });
-    res.status(201).json(owner);
+    const { fullName, phone, document } = req.body;
+    const owner = await OwnerService.createOwner({ fullName, phone, document });
+    return res.status(201).json(owner);
   } catch (error) {
-    next(error);
+    if (!error.status) {
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
+    return res
+      .status(error.status)
+      .json({ statusCode: error.status, message: error.message });
   }
 };
 
+const getOwnerByDocument = async (req, res) => {
+  try {
+    const { document } = req.body;
+    const owner = await OwnerService.getOwnerByDocument(document);
+    return res.status(200).json(owner);
+  } catch (error) {
+    if (!error.status) {
+      return res.status(500).json({ statusCode: 500, message: error.message });
+    }
+    return res
+      .status(error.status)
+      .json({ statusCode: error.status, message: error.message });
+  }
+};
 module.exports = {
   createOwner,
+  getOwnerByDocument,
 };
