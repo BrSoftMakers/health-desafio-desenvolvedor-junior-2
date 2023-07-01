1e65e679477be4f3114e862fd3affb93ca989808
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-01 13:30:49 -0300

    feat: add routa "/register-owner" no app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-
+const Router = require('./routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Servidor funcionando a todo vapor!!' });
+});
+
+app.use('/register-owner', Router.OwnerRouter);
 
 module.exports = app;
