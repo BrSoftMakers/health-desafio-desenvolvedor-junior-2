dcc51097bd57743b157565d40a707a8e2cb57fbc
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-01 22:10:24 -0300

    refactor: adiciona o componete Link do react-router-dom

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,15 +1,25 @@
+import { Link } from 'react-router-dom';
 export function Navbar() {
   return (
     <nav className="bg-yellow-400 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-gray-900 font-bold text-xl">Logo</div>
         <div className="flex space-x-4">
-          <a href="#" className="text-gray-900 font-semibold hover:text-gray-600">
+          <Link to="/" className="text-gray-900 font-semibold hover:text-gray-600">
+            Home
+          </Link>
+          <Link
+            to="/register-pet"
+            className="text-gray-900 font-semibold hover:text-gray-600"
+          >
             Cadastro
-          </a>
-          <a href="#" className="text-gray-900 font-semibold hover:text-gray-600">
+          </Link>
+          <Link
+            to="/list-pets"
+            className="text-gray-900 font-semibold hover:text-gray-600"
+          >
             Lista de Pets
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
