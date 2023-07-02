ae1a093a1b2133c1e68db1f95918f5db63092bfe
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-01 21:53:19 -0300

    feat: cria a home page

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold text-center text-slate-900">
+        Welcome to <span className="text-slate-500">Next.js</span> +{' '}
+        <span className="text-slate-500">Tailwind CSS</span>!
+      </h1>
+      <p className="mt-4 text-xl text-center text-slate-700">
+        This is a starter template for your next project.
+      </p>
+    </div>
+  );
+}
