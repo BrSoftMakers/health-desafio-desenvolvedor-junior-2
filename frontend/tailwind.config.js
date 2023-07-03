26e4ca5468a9ff11c48560f55f65505975268a82
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-03 11:26:20 -0300

    sytele: adicina configuração de estilo para background-image ao arquivo de configuração do tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-image': "url('src/assets/Rectangle1.png'), url('src/assets/Rectangle2.png')",
+        // 'rigth-image': "url('./assets/Rectangle2.png')",
+      },
+      backgroundPosition: {
+        'left-rigth': 'left, right',
+      },
+      backgroundSize: {
+        'leftS-rigthS': '50%, 50%',
+      },
+      colors: {
+        'bg-rgba': 'rgba(57, 57, 57, 0.50)',
+        primary: '#FFB800',
+      },
+    },
   },
   plugins: [],
 };
