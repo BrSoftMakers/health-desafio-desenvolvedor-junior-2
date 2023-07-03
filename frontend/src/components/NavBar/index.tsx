e2c8e70a7e5a3b5d5849728399e43c2aee9ad734
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-03 11:36:12 -0300

    styele: mudança da cor dos itens da navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
 export function Navbar() {
   return (
-    <nav className="bg-yellow-400 p-4">
+    <nav className="bg-primary p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-gray-900 font-bold text-xl">Logo</div>
+        <div className="text-gray-50 font-bold text-xl">Pet Health</div>
         <div className="flex space-x-4">
-          <Link to="/" className="text-gray-900 font-semibold hover:text-gray-600">
+          <Link to="/" className="text-gray-50 font-semibold hover:text-gray-600">
             Home
           </Link>
           <Link
             to="/register-pet"
-            className="text-gray-900 font-semibold hover:text-gray-600"
+            className="text-gray-50 font-semibold hover:text-gray-600"
           >
             Cadastro
           </Link>
           <Link
             to="/list-pets"
-            className="text-gray-900 font-semibold hover:text-gray-600"
+            className="text-gray-50 font-semibold hover:text-gray-600"
           >
             Lista de Pets
           </Link>
