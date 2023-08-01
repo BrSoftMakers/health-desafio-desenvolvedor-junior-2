e4a0dd640ed0c91f53962101e0bdd27b48bd0a85
Kawê <84145470+kawearaujo@users.noreply.github.com>
2023-08-01 02:39:26 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-psql "postgres://userpostgres:a8xefQjyWOT3bIOI68ryw2oEYBkezELb@dpg-cj48f0tgkuvsl0ffg86g-a/petshop_nr7r";
+
 const sequelize = new Sequelize("postgres://userpostgres:a8xefQjyWOT3bIOI68ryw2oEYBkezELb@dpg-cj48f0tgkuvsl0ffg86g-a/petshop_nr7r");
  
 module.exports = sequelize;
