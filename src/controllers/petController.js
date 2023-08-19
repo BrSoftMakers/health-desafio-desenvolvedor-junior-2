625bcda531d03133485ebc466492520681123f3a
Anthony4Dev <anthony.islan2005@gmail.com>
2023-08-18 23:27:04 -0300

    função de adicionar pet com tratamento de erro

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -8,3 +8,16 @@ const pool = new Pool({
     port: 5432,
 });
 
+const adicionarPet = async (req, res) => {
+    const { nome, idade, tipo, raca, nome_dono, telefone_dono } = req.body;
+    const query = 'INSERT INTO pets (nome, idade, tipo, raca, nome_dono, telefone_dono) VALUES ($1, $2, $3, $4, $5, $6)';
+
+
+    try {
+        await pool.query(query, [nome, idade, tipo, raca, nome_dono, telefone_dono]);
+        res.status(201).json({message: 'Pet adicionado com sucesso!'});
+    }catch (error) {
+        console.error('Erro ao adicionar o pet', error);
+        res.status(500).json({error: 'Erro ao adicionar o pet.'});
+    }
+};
