470df3236cf924eac51080925b5a454ce5027a4b
Anthony4Dev <anthony.islan2005@gmail.com>
2023-08-18 23:04:20 -0300

    rotas adicionadas

---
+++ src/rotas/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const petController = require('../models/petController');
+
+router.post('/adicionar-pet', petController.adicionarPet);
+router.get('/listar-pets', petController.listarPets);
+router.get('/visualizar-pet/:id', petController.visualizarPetPorId);
+router.put('/editar-pet/:id', petController.editarPetPorId);
+router.delete('/excluir-pet/:id', petController.excluirPetPorId);
+
+module.exports = router;
