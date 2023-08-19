341a1d4e63de2d2f6e15843d4546191b9c6d74b2
Anthony4Dev <anthony.islan2005@gmail.com>
2023-08-18 22:40:09 -0300

    refatorando o server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,3 @@
-const express = require("express")
-
-const app = express()
-
-app.use(express.json())
-
-app.get("/pegar", (req, res) => {
-
-    return res.json("servidor online");
-
-}); 
-
-app.listen(3333, () => console.log("Servidor online na porta 3333"))
+const express = require('express');
+const cors = require('cors');
+const app = express();
