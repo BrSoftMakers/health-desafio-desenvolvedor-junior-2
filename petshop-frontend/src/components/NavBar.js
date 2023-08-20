0d29cf402ea282b0889c571d61d2b70777a85316
Anthony4Dev <anthony.islan2005@gmail.com>
2023-08-20 11:30:28 -0300

    navbar feita

--- petshop-frontend/src/components/NavBar.js
+++ petshop-frontend/src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './styles/Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
       <ul>
         <li>
           <Link to="/">Home</Link>
