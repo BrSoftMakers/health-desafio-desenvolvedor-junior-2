18181fd79d6b745feec45e3559433f26ad76864d
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2023-09-13 10:54:29 -0300

    Update cpf.pipe.ts

--- frontend/petshop/src/app/pipes/cpf.pipe.ts
+++ frontend/petshop/src/app/pipes/cpf.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CpfFormatPipe implements PipeTransform {
   transform(value: string): string {
     // Remove os pontos e espaços do CPF
-    console.log(value);
+    
     const cpf = value.replace(/\D/g, '');
     return cpf;
   }
